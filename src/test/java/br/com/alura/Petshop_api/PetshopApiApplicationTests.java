8effffc054aa7ec6640bc44ff1fc6dabe8e4fdb3
wesley <wesleydsilva96@gmail.com>
2025-03-17 18:26:57 -0300

    criando projeto petshop-api

---
+++ src/test/java/br/com/alura/Petshop_api/PetshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.Petshop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
